924f62b4e668090ba044a8dd396e0528a2f0a226
user <darbinyan.199229@gmail.com>
2023-03-19 00:00:31 +0400

    test

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,11 @@ public class Main {
         human1.year = 1992;
         human1.year = 78787;
         human1.year = 78787;
+        human1.year = 78787;
+        human1.year = 78787;
+        human1.year = 78787;
+        human1.year = 78787;
+        human1.year = 78787;
 
 
         Human human2 = new Human();
