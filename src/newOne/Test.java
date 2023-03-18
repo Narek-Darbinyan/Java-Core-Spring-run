0b8a768f10541e46ca90b71dfb9a25e8a267297d
user <darbinyan.199229@gmail.com>
2023-03-19 00:05:40 +0400

    test4

--- src/newOne/Test.java
+++ src/newOne/Test.java
@@ -2,9 +2,6 @@ package newOne;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(
-                "test"
-        );
-        System.out.println(1+2);
+        System.out.println("ytea");
     }
 }
